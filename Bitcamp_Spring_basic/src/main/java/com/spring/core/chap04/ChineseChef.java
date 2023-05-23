8333d825ebed644066a4a818f97751758988ffba
신현섭 <sinh8492@naver.com>
2023-05-23 16:41:37 +0900

    commit

---
+++ Bitcamp_Spring_basic/src/main/java/com/spring/core/chap04/ChineseChef.java
@@ -0,0 +1,13 @@
+package com.spring.core.chap04;
+
+import org.springframework.stereotype.Component;
+
+
+// JapaneseChef를 참고해서 컨테이너에 등록 시켜주시고, 그림에도 반영 해주세요.
+@Component("cc")
+public class ChineseChef implements Chef {
+    @Override
+    public void cook() {
+        System.out.println("중식 경력 30년 천하오밍입니다.");
+    }
+}
